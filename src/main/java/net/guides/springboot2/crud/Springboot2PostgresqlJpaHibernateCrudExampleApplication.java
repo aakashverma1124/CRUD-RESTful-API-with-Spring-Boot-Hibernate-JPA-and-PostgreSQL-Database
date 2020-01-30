de8a9610ba68151649982eb463a35729af44b81f
aakashverma1124 <info.aakash11@gmail.com>
2020-01-30 22:14:56 +0530

    First commit

---
+++ src/main/java/net/guides/springboot2/crud/Springboot2PostgresqlJpaHibernateCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package net.guides.springboot2.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2PostgresqlJpaHibernateCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2PostgresqlJpaHibernateCrudExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/guides/springboot2/crud/Springboot2PostgresqlJpaHibernateCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.guides.springboot2.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2PostgresqlJpaHibernateCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
